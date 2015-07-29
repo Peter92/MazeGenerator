a63105e77be85a17836fa3bd2765c0851f370e68
Peter <Peter92@users.noreply.github.com>
2015-07-29 02:51:54 +0100

    Track the fork origins

--- MazeGen.py
+++ MazeGen.py
@@ -18,13 +18,15 @@ def calculate_directions(dimensions):
     return tuple(direction_list)
 
 
+
 class MazeGen(object):
     def __init__(self):
-        self.fork_dict = {}
-        #self.fork_dict[coordinate] = (sequence_number, fork_number, size)
+        self.point_dict = {}
+        self.fork_list = []
+        #self.point_dict[coordinate] = (sequence_number, fork_number, size)
         self.failed_attempts = 0
     
-    def calculate(self, fork_length, fork_number, dimensions, fork_start=None,
+    def generate(self, fork_length, fork_number, dimensions, fork_start=None,
                   direction_retries=4):
         
         if not isinstance(fork_start, (tuple, list)) or len(fork_start) != dimensions:
@@ -33,14 +35,17 @@ class MazeGen(object):
         #Initial calculations
         r_choice = random.choice
         self.direction_list = calculate_directions(dimensions)
-        self.fork_dict[tuple(fork_start)] = (0, 0, 1)
+        self.point_dict[tuple(fork_start)] = (0, -1, 1)
         range_fork_number = tuple(range(fork_number))
         range_fork_length = tuple(range(fork_length))
+        add_fork = self.fork_list.append
         
         
         for f_n in range_fork_number:
-            point_current = r_choice(fork_dict.keys())
-            point_sequence = point_current[0]
+            point_current = r_choice(self.point_dict.keys())
+            point_sequence = self.point_dict[point_current][0]
+            point_fork = self.point_dict[point_current][1]
+            add_fork((point_fork, point_sequence))
             
             for f_l in range_fork_length:
                 
@@ -49,28 +54,29 @@ class MazeGen(object):
                 if point_next is not None:
                     point_sequence += 1
                     point_current = point_next
-                    self.fork_dict[point_current] = (point_sequence, f_n, 1)
+                    self.point_dict[point_current] = (point_sequence, f_n, 1)
                     
                     
-        return self.fork_dict
-    
+        return (self.point_dict, tuple(self.fork_list))
+        
+        
     def _new_direction(self, point_current, direction_retries):
         for i in range(direction_retries):
             new_direction = r_choice(self.direction_list)
             new_coordinates = tuple(x+y for x, y in zip(point_current, new_direction))
-            if new_coordinates not in self.fork_dict:
+            if new_coordinates not in self.point_dict:
                 return new_coordinates
             self.failed_attempts += 1
 
 
 fork_length = 10
-fork_number = 1
+fork_number = 3
 dimensions = 2
-fork_dict = MazeGen().calculate(fork_length, fork_number, dimensions)
+point_dict, fork_list = MazeGen().generate(fork_length, fork_number, dimensions)
 
 
 #Debug list
-sorted_dict = sorted(fork_dict.items(), key=operator.itemgetter(1))
+sorted_dict = sorted(point_dict.items(), key=operator.itemgetter(1))
 for key, value in sorted_dict:
-    if value[1] == 0:
-        print key, value
+    print key, value
+print fork_list
