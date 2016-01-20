d23255648da95a4eefae9aa4ae4152a4867e45a7
Peter <Peter92@users.noreply.github.com>
2016-01-20 18:19:31 +0000

    Added pathfinding between two points

--- MazeGen.py
+++ MazeGen.py
@@ -30,6 +30,16 @@ class GenerationCore:
                 directions.append([j if i == n else 0 for n in self.range])
         return directions
 
+def format_location(coordinate, dx=0.0, dy=0.0, dz=0.0):
+    """Turn coordinates into 3D."""
+    num_coordinates = len(coordinate)
+    if num_coordinates == 1:
+        return (coordinate[0], dy, dz)
+    elif num_coordinates == 2:
+        return (coordinate[0], coordinate[1], dz)
+    else:
+        return tuple(coordinate[:3])
+        
 def collision_check(gc_instance, location, size, bounds=None):
     if bounds:
         for i in gc_instance.range:
@@ -44,7 +54,35 @@ def collision_check(gc_instance, location, size, bounds=None):
         if distance_total < pow(size_total, 2):
             return True
     return False
+
+def recursive_pathfind(start, end, node_list, path=[], reverse=True, last_id=None):
+    path = path + [start]
     
+    #Path complete
+    if start == end:
+        return path
+        
+    #Search parents
+    if reverse:
+        parent = node_list[start].parent
+        if parent is not None:
+            found_path = recursive_pathfind(parent, end, node_list, path=path, reverse=True, last_id=start)
+            if found_path is not None:
+                return found_path
+                
+    #Search children
+    for node_id in node_list[start].children:
+        if node_id != last_id:
+            found_path = recursive_pathfind(node_id, end, node_list, path=path, reverse=False, last_id=start)
+            if found_path is not None:
+                return found_path
+    return None
+
+def draw_path(node_list):
+    curve_points = [format_location(node.location) for node in node_list]
+    pm.curve(p=curve_points, d=5)
+
+
 class MayaDraw:
     def __init__(self, gc_instance):
         self._gc = gc_instance
@@ -59,6 +97,8 @@ class MayaDraw:
             if id in self._cubes:
                 pm.delete(id)
             self._cubes[node.id] = new_cube
+            pm.addAttr(new_cube, sn='gen_id', ln='GenerationID', min=0, at='long')
+            pm.setAttr('{}.gen_id'.format(new_cube), id)
     
     def curves(self):
         curve_list = []
@@ -74,27 +114,24 @@ class MayaDraw:
             curve_list[-1].append(node.location)
             
         for curves in curve_list:
-            if self._gc.dimensions == 2:
-                curves_3d = [(i[0], i[1], 0.0) for i in curves]
-            else:
-                curves_3d = [tuple(i[:3]) for i in curves]
             if len(curves) > 1:
-                pm.curve(p=curves_3d, d=1)
+                converted_coordinates = [format_location(coordinate) for coordinate in curves]
+                pm.curve(p=converted_coordinates, d=1)
         
 
-generation = GenerationCore(2)
+generation = GenerationCore(3)
 
-max_nodes = 10
+max_nodes = 1000
 min_nodes = 0
 max_fails = 500
-max_length = 1
+max_length = 20
 
 start_size = 0.5
-size_reduction = 0.9
+size_reduction = 0.98
 min_size = 0.01
 
-#bounds = ((-1, -1, -1), (10, 10, 10))
-bounds = None
+bounds = ((-1, -1, -1), (4, 4, 4))
+#bounds = None
 
 
 
@@ -146,3 +183,7 @@ while (total_nodes + failed_nodes < max_nodes
 md = MayaDraw(generation)
 md.cubes()
 
+start = 0
+end = generation.nodes[-1].id
+node_list = [generation.nodes[node_id] for node_id in recursive_pathfind(start, end, generation.nodes)]
+draw_path(node_list)
